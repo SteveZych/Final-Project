880314b527b122c77de8f9421340edf91f46f333
gldn-god <78330405+gldn-god@users.noreply.github.com>
2021-08-16 21:13:38 -0600

    flask app working for first two routes

--- app.py
+++ app.py
@@ -8,30 +8,30 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/tables")  
-def tables():  
-    con = sql.connect("hr.sqlite")
+@app.route("/tables")
+def tables():
+    con = sql.connect("data/hr.sqlite")
     con.row_factory = sql.Row
     cur = con.cursor()
-    cur.execute("select * from employees")
+    cur.execute("select * from employee")
     rows = cur.fetchall()
     return render_template("tables.html", rows = rows)
 
-@app.route('/terminations')
-def terminations():
-    # Display table of terminations
-    # Form for editing existing employees for termination
-    return render_template('terminations.html')
+# @app.route('/terminations')
+# def terminations():
+#     # Display table of terminations
+#     # Form for editing existing employees for termination
+#     return render_template('terminations.html')
 
-@app.route('/newHires')
-def terminations():
-    # Display table of most recent hires
-    # Form for adding new employees
-    return render_template('newHires.html')
+# @app.route('/newHires')
+# def terminations():
+#     # Display table of most recent hires
+#     # Form for adding new employees
+#     return render_template('newHires.html')
 
-@app.route('/mlModel')
-def learning():
-    # description of model and display code?
-    # graph of data 
-    # algorithm from scikitlearn
-    return render_template('mlModel.html')
+# @app.route('/mlModel')
+# def learning():
+#     # description of model and display code?
+#     # graph of data 
+#     # algorithm from scikitlearn
+#     return render_template('mlModel.html')
